59789e5fad3fd18d9154e89885c5ab2647ad7dfa
Eshar <16heereshar@gmail.com>
2024-12-23 02:18:43 +0000

    sadf asd

--- client/src/pages/AdminPanel.jsx
+++ client/src/pages/AdminPanel.jsx
@@ -95,6 +95,7 @@ const AdminPanel = ({ videos, setVideos, fetchVideos }) => {
                     },
                 }
             );
+            console.log("response = " + response.data);
             console.log("URL = " + response.data.image_key);
             return response.data.image_key; // The URL of the uploaded image
         } catch (error) {
