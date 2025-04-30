bbef0c95051198bf7d208d5306b3db31fb420624
Eshar Heer <ec241019@qmul.ac.uk>
2025-04-30 15:36:54 +0100

    added secure for https

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -46,7 +46,7 @@ const loginUser = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
+            secure: process.env.NODE_ENV === "production",
             sameSite: "Strict",
         });
 
